4f573f8dc21e92563532727bc699b42a1836996d
Mootjuh <haddouti_m@live.nl>
2020-05-03 20:15:27 +0200

    Made wildcard input values be saveable if they have a type assigned to them. Meaning if your wildcard input is a float and you have a value input, it will save and load that value properly.

--- Source/RuntimeBlueprints/RuntimeBpJsonLibrary.cpp
+++ Source/RuntimeBlueprints/RuntimeBpJsonLibrary.cpp
@@ -368,7 +368,20 @@ void URuntimeBpJsonLibrary::UpdateRuntimeScriptNodeDefaults(FRuntimeBpJsonFormat
 					}
 					default:
 					{
-						InputPin.VariableType = DefaultObject->InputPins[UsedIndex].VariableType;
+						// Wildcards have their used variable type stored in the pin index, however only when the pin isn't connected (node index = -1)
+						// The index stored in the pin index is the enum index from EVariableTypes
+						// We use PinIndex instead of UsedIndex here because we're not dealing with DefaultObject
+						if (SaveableNodes[NodeIndex].i[PinIndex].p > -1 && SaveableNodes[NodeIndex].i[PinIndex].n == -1)
+						{
+							EVariableTypes WildCardType = static_cast<EVariableTypes>(SaveableNodes[NodeIndex].i[PinIndex].p);
+							InputPin.VariableType = WildCardType;
+							SaveableNodes[NodeIndex].i[PinIndex].p = -1;
+							InputPin.ConnectedPinIndex = -1;
+						}
+						else
+						{
+							InputPin.VariableType = DefaultObject->InputPins[UsedIndex].VariableType;
+						}
 						break;
 					}
 				}
